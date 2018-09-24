e16c00097c204af5b1de3ffa18d7eb882f8acb09
Aek <>
2018-09-24 16:36:22 +0700

    update

--- TTP APP/ViewController.swift
+++ TTP APP/ViewController.swift
@@ -12,9 +12,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    let imgArray = [UIImage(named: "booking"), UIImage(named: "rentalcars")]
-    let dataArray = ["Booking", "Rentalcars"]
-    let urlArray = ["https://www.booking.com/index.en-gb.html?label=gen173nr-1DCAEoggJCAlhYSDNYBGjdAYgBAZgBLrgBB8gBDNgBA-gBAZICAXmoAgM;sid=4c71fdcc4ce8dae4969e62c107a7553c;keep_landing=1&sb_price_type=total&", "https://www.rentalcars.com"]
+    let imgArray = [UIImage(named: "booking"), UIImage(named: "rentalcars"), UIImage(named: "klook"), UIImage(named: "bellugg"), UIImage(named: "12goasia"), UIImage(named: "grab"), UIImage(named: "pamba"), UIImage(named: "now")]
+    let dataArray = ["Accomodations", "Car Rental", "Excursion", "Baggage Delivery", "TravelTickets", "Taxi", "RideShare", "Food Delivery"]
+    let urlArray = ["https://www.booking.com/index.en-gb.html?label=gen173nr-1DCAEoggJCAlhYSDNYBGjdAYgBAZgBLrgBB8gBDNgBA-gBAZICAXmoAgM;sid=4c71fdcc4ce8dae4969e62c107a7553c;keep_landing=1&sb_price_type=total&", "https://www.rentalcars.com", "https://www.klook.com", "https://system.bellugg.com/Booking/LuggageDelivery", "https://12go.asia", "https://www.grab.com/th/en/", "https://www.pambashare.com/", "https://www.now.in.th"]
     
     var estimateWidth = 160.0
     var cellMarginSize = 16.0
@@ -60,7 +60,8 @@ extension ViewController: UICollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ItemCell", for: indexPath) as! ItemCell
         cell.setData(image:self.imgArray[indexPath.row]!, text: self.dataArray[indexPath.row])
-        
+        cell.layer.cornerRadius = 10
+        cell.layer.masksToBounds = true
         return cell
     }
 }
